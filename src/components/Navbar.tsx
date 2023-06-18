4d90f642790fdba95c6427b14811de1890b7637e
jhigger <njohnjhigger@gmail.com>
2023-06-19 04:53:49 +0800

    navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+const Navbar = () => {
+	return (
+		<header className="navbar fixed top-0 z-40 w-full border-b border-base-100 bg-gray-950 backdrop-blur-sm">
+			<div className="container mx-auto px-4">
+				<div className="flex-1">
+					<Link className="text-xl text-gray-50" href="/">
+						Jhigger
+					</Link>
+				</div>
+				<nav className="flex-none">
+					<ul className="menu menu-horizontal gap-4 px-1">
+						<li>
+							<a>Link</a>
+						</li>
+						<li>
+							<a>Link 1</a>
+						</li>
+						<li>
+							<a>Link 2</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	);
+};
+
+export default Navbar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import Navbar from "~/components/Navbar";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Navbar />
+			<Component {...pageProps} />
+		</>
+	);
 };
 
 export default MyApp;
