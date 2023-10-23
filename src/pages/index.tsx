01fb2afb7b10c8e31ced3abed0e5dabe2d5c8adc
jhigger <njohnjhigger@gmail.com>
2023-10-23 10:37:39 +0800

    fix hover color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ const Sides = () => {
 	return (
 		<div className="container fixed bottom-0 left-0 right-0 z-10 mx-auto hidden justify-between px-8 lg:flex">
 			<div className="w-10 flex-col items-center justify-end after:mx-auto after:h-48 after:w-px after:bg-gray-500 lg:flex">
-				<ul className="flex flex-col gap-2 p-0 text-2xl last:mb-6 [&>*:hover]:-translate-y-1 [&>*:hover]:text-white [&>*]:m-0 [&>*]:p-2 [&>*]:transition-all [&>*]:duration-300 [&>*]:ease-in-out">
+				<ul className="flex flex-col gap-2 p-0 text-2xl last:mb-6 [&>*:hover]:-translate-y-1 [&>*:hover]:text-black dark:[&>*:hover]:text-white [&>*]:m-0 [&>*]:p-2 [&>*]:transition-all [&>*]:duration-300 [&>*]:ease-in-out">
 					<li>
 						<a
 							href="https://github.com/jhigger"
@@ -57,7 +57,7 @@ const Sides = () => {
 				</ul>
 			</div>
 			<div className="w-10 flex-col items-center justify-end after:mx-auto after:h-24 after:w-px after:bg-gray-500 lg:flex">
-				<div className="flex flex-col last:mb-6 [&>*:hover]:-translate-y-1 [&>*:hover]:text-white [&>*]:transition-all [&>*]:duration-300 [&>*]:ease-in-out">
+				<div className="flex flex-col last:mb-6 [&>*:hover]:-translate-y-1 [&>*:hover]:text-black dark:[&>*:hover]:text-white [&>*]:transition-all [&>*]:duration-300 [&>*]:ease-in-out">
 					<a
 						className="m-0 p-2 text-xs tracking-widest [writing-mode:vertical-lr]"
 						href="mailto: njohnjhigger@gmail.com"
