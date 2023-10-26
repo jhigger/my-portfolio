1ee43ca31f58864db4dccb2d7452dbc1b14262c8
jhigger <njohnjhigger@gmail.com>
2023-10-26 18:41:44 +0800

    projects

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,41 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import { FiExternalLink, FiGithub } from "react-icons/fi";
-
-type FeaturedProjectType = {
-	image: string;
-	title: string;
-	description: string;
-	preview: string;
-	github: string;
-};
+import { featuredProjects, otherProjects } from "~/projects";
 
 const FeaturedProjects = () => {
-	const featuredProjects: FeaturedProjectType[] = [
-		{
-			image: "images/pizza_stack.jpg",
-			title: "Pizza Stack",
-			description:
-				"Designed an attractive and user-friendly interface using ReactJS and Chakra UI, added animations using Framer Motion, integrated a reservation form for franchising using Formik, and optimized the website for mobile devices.",
-			preview: "https://jhigger-pizzastack.netlify.app/",
-			github: "https://github.com/jhigger/pizzastack2",
-		},
-		{
-			image: "images/duckett_architecture.jpg",
-			title: "Duckett Architecture",
-			description:
-				"Developed a landing page for an architectural firm based in New Zealand to showcase their projects. I utilized NextJS, ReactJS and TailwindCSS to easily design and make it SEO friendly.",
-			preview: "https://www.duckettarchitecture.co.nz/",
-			github: "https://github.com/jhigger/duckett-archi",
-		},
-		{
-			image: "images/qtopia.jpg",
-			title: "Qtopia",
-			description: "Created a landing page for an NFT project.",
-			preview: "https://www.qtopia.io/",
-			github: "https://github.com/jhigger/qtopia",
-		},
-	];
-
 	return (
 		<section>
 			<h3 className="!mt-0 flex items-baseline justify-end gap-4">
@@ -97,30 +64,7 @@ const FeaturedProjects = () => {
 	);
 };
 
-type OtherProjectType = Omit<FeaturedProjectType, "image">;
-
 const OtherProjects = () => {
-	const projects: OtherProjectType[] = Array(10)
-		.fill(null)
-		.map((_, i) => {
-			if (i % 2) {
-				return {
-					title: "title title title",
-					description:
-						"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur, minima.",
-					preview: "",
-					github: "",
-				};
-			} else {
-				return {
-					title: "title title title",
-					description: "Lorem ipsum, dolor sit amet.",
-					preview: "",
-					github: "",
-				};
-			}
-		});
-
 	return (
 		<section>
 			<h3 className="row-span-full flex items-baseline justify-end gap-4">
@@ -128,37 +72,45 @@ const OtherProjects = () => {
 				Other Projects
 			</h3>
 			<ul className="prose-base grid w-full items-center gap-4 p-0 md:grid-cols-2 xl:grid-cols-3">
-				{projects.map((project) => {
-					return (
-						<li key={project.title} className="group h-full">
-							<div className="relative flex h-full flex-col overflow-clip rounded-lg bg-gray-500/5 p-8 shadow">
-								<div className="folder-effect"></div>
-								<h4 className="custom-underline !mt-0 border-b-[1px] border-transparent pr-2">
-									{project.title}
-								</h4>
-								<p>{project.description}</p>
-								<div className="flex h-full items-end gap-4">
-									<a
-										title="GitHub Link"
-										href={project.github}
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										<FiGithub />
-									</a>
-									<a
-										title="Preview Link"
-										href={project.preview}
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										<FiExternalLink />
-									</a>
+				{otherProjects
+					.map((project) => {
+						return (
+							<li key={project.title} className="group h-full">
+								<div className="relative flex h-full flex-col overflow-clip rounded-lg bg-gray-500/5 p-8 shadow">
+									<div className="folder-effect"></div>
+									<h4 className="custom-underline !mt-0 border-b-[1px] border-transparent pr-2">
+										<a
+											href={project.preview}
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											{project.title}
+										</a>
+									</h4>
+									<p>{project.description}</p>
+									<div className="flex h-full items-end gap-4">
+										<a
+											title="GitHub Link"
+											href={project.github}
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											<FiGithub />
+										</a>
+										<a
+											title="Preview Link"
+											href={project.preview}
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											<FiExternalLink />
+										</a>
+									</div>
 								</div>
-							</div>
-						</li>
-					);
-				})}
+							</li>
+						);
+					})
+					.reverse()}
 			</ul>
 		</section>
 	);
---
+++ src/projects.ts
@@ -0,0 +1,84 @@
+type FeaturedProjectType = {
+	image: string;
+	title: string;
+	description: string;
+	preview: string;
+	github: string;
+};
+
+export const featuredProjects: FeaturedProjectType[] = [
+	{
+		image: "images/pizza_stack.jpg",
+		title: "Pizza Stack",
+		description:
+			"Designed an attractive and user-friendly interface using ReactJS and Chakra UI, added animations using Framer Motion, integrated a reservation form for franchising using Formik, and optimized the website for mobile devices.",
+		preview: "https://jhigger-pizzastack.netlify.app/",
+		github: "https://github.com/jhigger/pizzastack2",
+	},
+	{
+		image: "images/duckett_architecture.jpg",
+		title: "Duckett Architecture",
+		description:
+			"Developed a landing page for an architectural firm based in New Zealand to showcase their projects. I utilized NextJS, ReactJS and TailwindCSS to easily design and make it SEO friendly.",
+		preview: "https://www.duckettarchitecture.co.nz/",
+		github: "https://github.com/jhigger/duckett-archi",
+	},
+	{
+		image: "images/qtopia.jpg",
+		title: "Qtopia",
+		description: "Created a landing page for an NFT project.",
+		preview: "https://www.qtopia.io/",
+		github: "https://github.com/jhigger/qtopia",
+	},
+];
+
+type OtherProjectType = Omit<FeaturedProjectType, "image">;
+
+export const otherProjects: OtherProjectType[] = [
+	{
+		title: "Master Of Gingerbread",
+		description:
+			"Website template for a bakery made with vanilla JS, HTML and CSS.",
+		github: "https://github.com/jhigger/MasterOfGingerbread",
+		preview: "https://masterofgingerbread.netlify.app/",
+	},
+	{
+		title: "Materialize",
+		description:
+			"Landing page template made with Materialize CSS framework by Google.",
+		github: "https://github.com/jhigger/Materialize",
+		preview: "https://jhigger-materialize.netlify.app/",
+	},
+	{
+		title: "Progressive Weather App",
+		description:
+			"A basic front end for querying data from the OpenWeatherMap API.",
+		github: "https://github.com/jhigger/PWA-Weather-App",
+		preview: "https://jhigger-weather-app.netlify.app/",
+	},
+	{
+		title: "Expense Tracker",
+		description:
+			"A note-taking app that calculates expenses in real-time using Firebase.",
+		github: "https://github.com/jhigger/expense-tracker",
+		preview: "https://expense-tracker-jhigger.vercel.app/",
+	},
+	{
+		title: "ArtisseAI",
+		description: "A simple page for a mobile application.",
+		github: "https://github.com/jhigger/artisse",
+		preview: "https://kairos-artisse.vercel.app/",
+	},
+	{
+		title: "Foxbyte",
+		description: "Landing page for an NFT project.",
+		github: "https://github.com/jhigger/foxbyte",
+		preview: "https://kairos-foxbyte.vercel.app/",
+	},
+	// {
+	// 	title: "",
+	// 	description: "",
+	// 	github: "",
+	// 	preview: "",
+	// },
+];
