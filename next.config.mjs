65a01d909efe498d514c5e48a9bbb552b35f1660
jhigger <njohnjhigger@gmail.com>
2023-10-28 11:51:59 +0800

    firebase hosting

--- next.config.mjs
+++ next.config.mjs
@@ -6,18 +6,15 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+	reactStrictMode: true,
 
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+	/**
+	 * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+	 * out.
+	 *
+	 * @see https://github.com/vercel/next.js/issues/41980
+	 */
+	output: "export",
 };
 
 export default config;
